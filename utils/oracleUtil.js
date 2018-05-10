507ad3c557431fd619b13dccfcb4cbe0260cee63
zy-xx <xx198742>
2018-05-10 22:20:46 +0800

    oracledb连接池 增加任务

--- utils/oracleUtil.js
+++ utils/oracleUtil.js
@@ -1,24 +1,32 @@
 var oracledb = require('oracledb');
+var config = require('../config').initConfig();
+var async = require("async");
+var oraclePool = null;
 
-var config = {
-    user: 'xingji',　　//用户名
-    password: 'xingji',　　//密码
-    //IP:数据库IP地址，PORT:数据库端口，SCHEMA:数据库名称
-    connectString: "192.168.1.81:1521/orcl"
+var initOracleConnectPool = function (getConnection) {
+    oraclePool = oracledb.createPool(config.connInfo, function (err, pool) {
+        getConnection(err, pool);
+    });
 };
 
-function execSql(sql, resutlHandle) {
-    oracledb.getConnection(
-        config,
-        function (err, connection) {
+execSql("select 1 from dual", function (result) {
+    console.log(result.rows);
+});
+
+function execSql(sql, resultHandle) {
+    async.series([function (getConnection) {
+        if (!oraclePool) {
+            initOracleConnectPool(getConnection);
+        }
+    }], function (err, pool) {
+        pool.getConnection(function (err, connection) {
             if (err) {
                 console.error(err.message);
                 setTimeout(function () {
-                    execSql(sql, resutlHandle);
+                    execSql(sql, resultHandle);
                 }, 10000);
                 return;
             }
-            // try {
             connection.execute(sql,
                 function (err, result) {
                     if (err) {
@@ -26,14 +34,47 @@ function execSql(sql, resutlHandle) {
                         doRelease(connection);
                         return;
                     }
-                    resutlHandle(result);
+                    resultHandle(result);
                 });
-            // } finally {
-            //     doRelease(connection);
-            // }
         });
+    });
 }
 
+// var config = {
+//     user: 'xingji',　　//用户名
+//     password: 'xingji',　　//密码
+//     //IP:数据库IP地址，PORT:数据库端口，SCHEMA:数据库名称
+//     connectString: "192.168.1.81:1521/orcl"
+// };
+//
+// function execSql(sql, resutlHandle) {
+//     oracledb.getConnection(
+//         config,
+//         function (err, connection) {
+//             if (err) {
+//                 console.error(err.message);
+//                 setTimeout(function () {
+//                     execSql(sql, resutlHandle);
+//                 }, 10000);
+//                 return;
+//             }
+//             // try {
+//             connection.execute(sql,
+//                 function (err, result) {
+//                     if (err) {
+//                         console.error(err.message);
+//                         doRelease(connection);
+//                         return;
+//                     }
+//                     resutlHandle(result);
+//                 });
+// //             // } finally {
+// //             //     doRelease(connection);
+// //             // }
+// //             // doRelease(connection);
+//         });
+// }
+//
 function doRelease(connection) {
     connection.close(
         function (err) {
